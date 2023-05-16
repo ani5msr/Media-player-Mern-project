5d57f47f4662ee71f0011f64cbff5cf8b98eaa6d
Anirudh Mishra <anirudh5mar@gmail.com>
2023-05-16 19:43:39 +0530

    Created api for get,post, and list

--- client/media/media-api.js
+++ client/media/media-api.js
@@ -13,3 +13,105 @@ const create = async (params, cred, media) => {
       console.log(err)
     }
   }
+  const listPopular = async (signal) => {
+    try {
+      let response = await fetch('/api/media/popular', {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+        return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
+  const listByUser = async (params) => {
+    try {
+      let response = await fetch('/api/media/by/'+ params.userId, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+        return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
+  const read = async (params, signal) => {
+    try {
+      let response = await fetch(config.serverUrl +'/api/media/' + params.mediaId, {
+      method: 'GET',
+      signal: signal
+    })
+      return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
+  const update = async (params, credentials, media) => {
+    try {
+      let response = await fetch('/api/media/' + params.mediaId, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: JSON.stringify(media)
+    })    
+      return await response.json()
+      } catch(err) {
+        console.log(err)
+      }
+  }
+  
+  const remove = async (params, credentials) => {
+    try {
+      let response = await fetch('/api/media/' + params.mediaId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })    
+    return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
+  const listRelated = async (params, signal) => {
+    try {
+      let response = await fetch('/api/media/related/'+ params.mediaId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+        return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
+  export {
+    create,
+    listPopular,
+    listByUser,
+    read,
+    update,
+    remove,
+    listRelated
+  }
+  
